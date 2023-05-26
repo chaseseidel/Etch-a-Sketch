787d67999c4cf4084912b25ea0973167b06fd021
chaseseidel <chaseseidel16@gmail.com>
2023-05-26 12:42:10 -0700

    Add resetGrid function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+//Grid variables
 const container = document.querySelector('.container');
 
+//Color and draw variables
+const colorSelector = document.getElementById('color-selector');
+
+let rainbow = false;
+let color = colorSelector.value;
+let click = false;
+
+//Grid Functions
 function createGrid(input) {
     for (let i = 0; i < input; i++) {
         const column = document.createElement('div');
@@ -27,12 +36,21 @@ function deleteGrid() {
     }
 }
 
-let color = 'black';
-let click;
+function resetGrid() {
+    const squares = container.querySelectorAll('.square');
+    squares.forEach(square => square.style.backgroundColor = 'white')
+}
+
+//Color and draw functions
 function squareDragEvent(element) {
     element.addEventListener('mousedown', () => {
         click = true;
-        element.style.backgroundColor = color;
+        if (rainbow) {
+            element.style.backgroundColor = rainbowColor();
+        }
+        else {
+            element.style.backgroundColor = color;
+        }
     })
     element.addEventListener('mouseover', () => {
         if (click && rainbow) {
@@ -47,25 +65,14 @@ function squareDragEvent(element) {
     })
 }
 
-let colorSelector = document.getElementById('color-selector');
 function colorPicker() {
     color = colorSelector.value;
+    rainbow = false;
 }
 
-let rainbow = false;
 function rainbowColor() {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
     return `rgb(${r}, ${g}, ${b})`;
 }
-
-const rainbowButton = document.getElementById('rainbow-button');
-rainbowButton.addEventListener('click', () => {
-    if (rainbow == false) {
-        rainbow = true;
-    }
-    else {
-        rainbow = false;
-    }
-})
