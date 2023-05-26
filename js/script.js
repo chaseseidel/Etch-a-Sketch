c5d88f0947470f116d18436b48c2463e26f3ffd1
chaseseidel <chaseseidel16@gmail.com>
2023-05-25 20:30:33 -0700

    Add grid and color in grid functions

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const container = document.querySelector('.container');
+
+let userInput = 100;
+for (let i = 0; i < userInput; i++) {
+    const column = document.createElement('div');
+    column.classList.add('column');
+
+    for (let j = 0; j < userInput; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        squareDragEvent(square);
+        column.appendChild(square);
+    }
+
+    container.appendChild(column);
+}
+
+let click;
+function squareDragEvent(element) {
+    element.addEventListener('mousedown', () => {
+        click = true;
+        element.style.backgroundColor = 'black';
+    })
+    element.addEventListener('mouseover', () => {
+        if (click) {
+            element.style.backgroundColor = 'black';
+        }
+    })
+    element.addEventListener('mouseup', () => {
+        click = false;
+    })
+}
