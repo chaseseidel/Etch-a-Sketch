3dcbe43badbc2d883fc65b409a988bfc5f3ca3b7
chaseseidel <chaseseidel16@gmail.com>
2023-05-26 12:45:25 -0700

    Remove UI.js file

--- js/UI.js
+++
@@ -1,7 +0,0 @@
-const defaultSize = document.getElementById('range').value;
-const slider = document.getElementById('range');
-slider.setAttribute('oninput', 'changeSize(this.value)');
-colorSelector.setAttribute('oninput', 'colorPicker()')
-
-createGrid(defaultSize);
-
