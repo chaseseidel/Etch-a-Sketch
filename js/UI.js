df7864747e7cc737087070df6da2c4db1e17e662
chaseseidel <chaseseidel16@gmail.com>
2023-05-26 01:49:49 -0700

    Add oninput attribute to colorSelector

--- js/UI.js
+++ js/UI.js
@@ -1,6 +1,7 @@
 const defaultSize = document.getElementById('range').value;
 const slider = document.getElementById('range');
 slider.setAttribute('oninput', 'changeSize(this.value)');
+colorSelector.setAttribute('oninput', 'colorPicker()')
 
 createGrid(defaultSize);
 
